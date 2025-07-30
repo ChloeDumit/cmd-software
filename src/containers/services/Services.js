02064980512a67f97965cf74dd973704f281996c
chloeDumit <dumitchloe@gmail.com>
2025-07-29 21:48:21 -0300

    fix

--- src/containers/services/Services.js
+++ src/containers/services/Services.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React  from "react";
 import "./services.scss";
 import { services } from "../../portfolio";
 import ServiceCard from "../../components/serviceCard/serviceCard";
